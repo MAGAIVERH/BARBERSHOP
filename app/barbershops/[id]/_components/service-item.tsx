cc32a2e0510b318897a1177220e2cf4781f3a004
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-02-10 19:48:23 -0300

    feat: add toast on booking success

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -8,11 +8,13 @@ import { Barbershop, Service  } from "@prisma/client";
 import { ptBR } from "date-fns/locale";
 import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { generateDayTimeList } from "../_helpers/hours";
 import { format, setHours, setMinutes } from "date-fns";
 import { saveBooking } from "../_actions/save-booking";
 import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 
 
@@ -26,12 +28,15 @@ interface ServiceItemProps {
 }
 
 const ServiceItem  = ({service, barbershop, isAuthenticated}: ServiceItemProps) => {
+
+  const router = useRouter();  
   
   const {data} =useSession();  
 
   const [date, setDate] = useState<Date | undefined>(undefined);
   const [hour, setHour] = useState<string | undefined>();
   const [submitIsLoading, setSubmitIsLoading] = useState(false);
+  const [sheetIsOpen, setSheetIsOpen] = useState(false);
 
   const handleDateClick = (date: Date | undefined) => {
     setDate(date);
@@ -67,6 +72,21 @@ const ServiceItem  = ({service, barbershop, isAuthenticated}: ServiceItemProps) 
             date: newDate,
             userId: (data.user as any).id,
         });
+
+        setSheetIsOpen(false);
+        setHour(undefined);
+        setDate(undefined);
+        toast("Reserva realizada com sucesso!", {
+            description: format(newDate, "'Para' dd 'de' MMMM 'às' HH':'mm '.' ", {
+            locale: ptBR,
+            }), 
+            action: {
+              label: "Visualizar",
+              onClick: () => router.push("/bookings"),
+            },
+          });
+
+
     } catch (error){
         console.error(error);
     } finally {
@@ -78,7 +98,10 @@ const ServiceItem  = ({service, barbershop, isAuthenticated}: ServiceItemProps) 
 
   const timeList = useMemo(() => {
     return date ? generateDayTimeList(date): []
-  }, [date]) 
+  }, [date]);
+
+
+
 
 
 
@@ -110,7 +133,7 @@ const ServiceItem  = ({service, barbershop, isAuthenticated}: ServiceItemProps) 
                 }).format(service.price)}
             </p>
             
-            <Sheet>
+            <Sheet open={sheetIsOpen} onOpenChange={setSheetIsOpen}>
                 <SheetTrigger asChild>
                     <Button variant="secondary" onClick={handleBookingClick}>
                         Reservar
